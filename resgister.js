f7c42682668aa90470efdae9dfc3e04175ef9034
Tarcizioo <tarcizioneto10@gmail.com>
2024-11-26 16:20:38 -0300

    update(register)

--- resgister.js
+++ resgister.js
@@ -3,32 +3,16 @@ document.getElementById('registerForm').addEventListener('submit', function(even
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    const profilePicture = document.getElementById('profilePicture').files[0];
     const errorMessage = document.getElementById('error-message');
 
     if (username && password) {
         // Armazenar dados do usuário no localStorage
-        const reader = new FileReader();
-        reader.onloadend = function() {
-            const userData = {
-                username: username,
-                password: password,
-                profilePicture: reader.result
-            };
-            localStorage.setItem('user', JSON.stringify(userData));
-            window.location.href = 'index.html'; // Redireciona para a página de login
+        const userData = {
+            username: username,
+            password: password
         };
-        if (profilePicture) {
-            reader.readAsDataURL(profilePicture);
-        } else {
-            const userData = {
-                username: username,
-                password: password,
-                profilePicture: null
-            };
-            localStorage.setItem('user', JSON.stringify(userData));
-            window.location.href = 'index.html'; // Redireciona para a página de login
-        }
+        localStorage.setItem('user', JSON.stringify(userData));
+        window.location.href = 'index.html'; // Redireciona para a página de login
     } else {
         errorMessage.textContent = 'Por favor, preencha todos os campos!';
     }
