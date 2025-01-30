cbc42581ba7481554015320da17f9d18ad6e6b2f
Tarcizioo <tarcizioneto10@gmail.com>
2025-01-30 09:54:35 -0300

    HHAHAHAHA

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-const editProfileBtn = document.getElementById('editProfileBtn');
+        const editProfileBtn = document.getElementById('editProfileBtn');
         const editProfileModal = document.getElementById('editProfileModal');
         const closeModalBtn = document.getElementById('closeModalBtn');
         const saveProfileBtn = document.getElementById('saveProfileBtn');
@@ -7,7 +7,13 @@ const editProfileBtn = document.getElementById('editProfileBtn');
         const emailInput = document.getElementById('emailInput');
         const avatarInput = document.getElementById('avatarInput');
         const userAvatar = document.getElementById('userAvatar');
-
+        const bannerInput = document.getElementById('bannerInput');
+        const userBanner = document.getElementById('userBanner');
+        const cropperModal = document.getElementById('cropperModal');
+        const overlay = document.getElementById('overlay');
+        const imageToCrop = document.getElementById('imageToCrop');
+        let cropper;
+        
         editProfileBtn.addEventListener('click', () => {
             editProfileModal.classList.add('show');
         });
@@ -35,7 +41,16 @@ const editProfileBtn = document.getElementById('editProfileBtn');
                 reader.readAsDataURL(avatarInput.files[0]);
             }
 
+            if (bannerInput.files && bannerInput.files[0]){
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    userBanner.src = e.target.result;
+                };
+                reader.readAsDataURL(bannerInput.files[0]);
+            }
+
             editProfileModal.classList.remove('show');
+            
         });
         document.getElementById('home').addEventListener('click', function() {
             window.location.href = 'home.html';
